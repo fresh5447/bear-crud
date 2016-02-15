74213285d0c20a9a968c8d27c3d1ecc0ab67f905
fresh5447 <doug@kosmojo.com>
2016-02-15 10:26:24 -0700

    working calculator function

---
+++ myCalculator.js
@@ -0,0 +1,10 @@
+//IMPORT THE OTHER FILE
+var Calculator = require('./calculator');
+
+//CREATE A NEW CALCULATOR
+var myCalculator = new Calculator();
+console.log(myCalculator);
+
+
+
+//TEST THE SUM FUNCTION
