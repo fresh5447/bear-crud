85672101030bc3531231aed2b2715da23156e664
fresh5447 <doug@kosmojo.com>
2016-02-15 10:39:55 -0700

    implement return and console log effectively

--- myCalculator.js
+++ myCalculator.js
@@ -3,8 +3,8 @@ var Calculator = require('./calculator');
 
 //CREATE A NEW CALCULATOR
 var myCalculator = new Calculator();
-console.log(myCalculator);
-
-
 
 //TEST THE SUM FUNCTION
+console.log(myCalculator.sum(7,7));
+console.log(myCalculator.subtract(10,5));
+console.log(myCalculator.subtract(5,10));
