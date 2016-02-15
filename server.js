737c3e55752a411a583d2c762ecee72da58e0e2a
fresh5447 <doug@kosmojo.com>
2016-02-15 12:00:03 -0700

    get bear api endpoint working

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var app = express();
 
+var monte = {name: 'monte', id: 0, age: 93, gender: 'male'};
+var smokey = {name: 'smokey', id: 1, age: 17, gender: 'male'};
+
+var bears = [monte, smokey];
+
 app.get('/api/bears', function(req, res){
-  res.json({title: 'does it work?', body: 'I hope so..'})
+  res.json(bears);
 });
+
 
 app.listen(3000, function(){
   console.log('the server is up and running..')
